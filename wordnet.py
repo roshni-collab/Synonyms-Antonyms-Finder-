c485eb5e83e22d592073cd8011256d065361ed63
Roshni Gupta <roshni.g@turing.com>
2025-03-13 18:20:50 +0530

    looping

--- wordnet.py
+++ wordnet.py
@@ -5,3 +5,8 @@ nltk.download('wordnet')
 word = input("Enter a word: ")
 
 synsets = wordnet.synsets(word)
+
+synonyms = set()
+for syn in synsets:
+    for lemma in syn.lemmas():
+        synonyms.add(lemma.name())
